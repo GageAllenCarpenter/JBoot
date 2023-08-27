ae06f4d7d660387e3fad59e7c7adffd1fd89e0a5
Gage Allen Carpenter <gageallencarpenter@gmail.com>
2023-08-27 17:37:24 -0400

    Reboot scheduling has been added

---
+++ src/main/java/io/github/gageallencarpenter/RebootScheduler.java
@@ -0,0 +1,79 @@
+package io.github.gageallencarpenter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+/**
+ * A utility class for scheduling and executing system reboots with optional tasks using PowerShell.
+ */
+public class RebootScheduler {
+    private final Powershell powershell;
+
+    /**
+     * Creates a new RebootScheduler instance.
+     *
+     * @param powershell The Powershell instance to execute PowerShell commands.
+     */
+    public RebootScheduler(Powershell powershell){
+        this.powershell = powershell;
+    }
+
+    /**
+     * Schedules an immediate system reboot.
+     */
+    public void scheduleImmediateReboot(){
+        executeShutdown(0);
+    }
+
+    /**
+     * Schedules a system reboot at a specific date and time.
+     *
+     * @param rebootDate The date and time to schedule the reboot.
+     */
+    public void scheduleRebootAt(Date rebootDate){
+        long secondsUntilReboot = calculateSecondsUntilReboot(rebootDate);
+        executeShutdown(secondsUntilReboot);
+    }
+
+    /**
+     * Schedules a system reboot at a specific date and time with a program to run on startup.
+     *
+     * @param rebootDate The date and time to schedule the reboot.
+     * @param program The program to run on startup.
+     */
+    public void scheduleProgrammedReboot(Date rebootDate, String program) {
+        scheduleTask(program);
+        scheduleRebootAt(rebootDate);
+    }
+
+    private void scheduleTask(String program) {
+        String script = generateScheduleScript(program);
+        powershell.execute(script);
+    }
+
+    private String generateScheduleScript(String program) {
+        return "$Action = New-ScheduledTaskAction -Execute "+ "\"" + program + "\"" +"\n" +
+                "$Trigger = New-ScheduledTaskTrigger -AtLogon\n" +
+                "Register-ScheduledTask " + "\"" + program + " reboot" + "\"" + " -Action $Action -Trigger $Trigger -RunLevel Highest";
+    }
+
+    private long calculateSecondsUntilReboot(Date rebootDate) {
+        Instant now = Instant.now();
+        Instant targetInstant = rebootDate.toInstant();
+        Duration duration = Duration.between(now, targetInstant);
+        return duration.getSeconds();
+    }
+
+    private void executeShutdown(long secondsUntilReboot) {
+        try {
+            powershell.execute("shutdown -r -t " + secondsUntilReboot);
+        } catch (Exception e) {
+            handleException(e);
+        }
+    }
+
+    private void handleException(Exception e) {
+        // Log the error or perform appropriate error handling
+        System.err.println("An error occurred: " + e.getMessage());
+    }
+}
